517ecabf1e4154ca95c816b4ecbe296bd28a811c
Vibhav Pant <vibhavp@gmail.com>
2023-07-13 19:23:21 +0530

    Rewrite SignatureParser using nom.

--- zvariant/src/signature_parser.rs
+++ zvariant/src/signature_parser.rs
@@ -1,4 +1,14 @@
-use std::ops::{Bound, RangeBounds};
+use std::ops::{Bound, RangeBounds, RangeFrom, RangeTo};
+
+use nom::{
+    branch::alt,
+    character::complete::{char, satisfy},
+    combinator::{cut, recognize},
+    error::{ContextError, ParseError},
+    multi::many1_count,
+    sequence::{delimited, pair, preceded},
+    AsChar, IResult, InputIter, InputLength, Offset, Slice,
+};
 
 use crate::{subslice, Basic, ObjectPath, Result, Signature, STRUCT_SIG_END_CHAR};
 
@@ -121,179 +131,69 @@ impl<'s> SignatureParser<'s> {
 
     /// Get the next signature but don't increment the position.
     pub fn next_signature(&self) -> Result<Signature<'_>> {
-        match self
-            .signature()
-            .as_bytes()
-            .first()
-            .map(|b| *b as char)
-            .ok_or_else(|| -> crate::Error {
-                serde::de::Error::invalid_length(0, &">= 1 character")
-            })? {
-            u8::SIGNATURE_CHAR
-            | bool::SIGNATURE_CHAR
-            | i16::SIGNATURE_CHAR
-            | u16::SIGNATURE_CHAR
-            | i32::SIGNATURE_CHAR
-            | u32::SIGNATURE_CHAR
-            | i64::SIGNATURE_CHAR
-            | u64::SIGNATURE_CHAR
-            | f64::SIGNATURE_CHAR
-            | <&str>::SIGNATURE_CHAR
-            | ObjectPath::SIGNATURE_CHAR
-            | Signature::SIGNATURE_CHAR
-            | VARIANT_SIGNATURE_CHAR => Ok(self.signature_slice(0, 1)),
-            #[cfg(unix)]
-            Fd::SIGNATURE_CHAR => Ok(self.signature_slice(0, 1)),
-            ARRAY_SIGNATURE_CHAR => self.next_array_signature(),
-            STRUCT_SIG_START_CHAR => self.next_structure_signature(),
-            DICT_ENTRY_SIG_START_CHAR => self.next_dict_entry_signature(),
-            #[cfg(feature = "gvariant")]
-            MAYBE_SIGNATURE_CHAR => self.next_maybe_signature(),
-            c => Err(serde::de::Error::invalid_value(
-                serde::de::Unexpected::Char(c),
-                &"a valid signature character",
-            )),
+        match signature::<_, nom::error::Error<_>>(self.signature().as_str()) {
+            Ok((_, output)) => Ok(self.signature_slice(0, output.len())),
+            Err(e) => Err(crate::Error::Message(e.to_string())),
         }
-    }
-
-    fn next_single_child_type_container_signature(
-        &self,
-        expected_sig_prefix: char,
-    ) -> Result<Signature<'_>> {
-        let signature = self.signature();
-
-        if signature.len() < 2 {
-            return Err(serde::de::Error::invalid_length(
-                signature.len(),
-                &">= 2 characters",
-            ));
-        }
-
-        // We can't get None here cause we already established there is are least 2 chars above
-        let c = signature
-            .as_bytes()
-            .first()
-            .map(|b| *b as char)
-            .expect("empty signature");
-        if c != expected_sig_prefix {
-            return Err(serde::de::Error::invalid_value(
-                serde::de::Unexpected::Char(c),
-                &expected_sig_prefix.to_string().as_str(),
-            ));
-        }
-
-        // There should be a valid complete signature after 'a' but not more than 1
-        let child_parser = self.slice(1..);
-        let child_len = child_parser.next_signature()?.len();
-
-        Ok(self.signature_slice(0, child_len + 1))
-    }
-
-    fn next_array_signature(&self) -> Result<Signature<'_>> {
-        self.next_single_child_type_container_signature(ARRAY_SIGNATURE_CHAR)
-    }
-
-    #[cfg(feature = "gvariant")]
-    fn next_maybe_signature(&self) -> Result<Signature<'_>> {
-        self.next_single_child_type_container_signature(MAYBE_SIGNATURE_CHAR)
-    }
-
-    fn next_structure_signature(&self) -> Result<Signature<'_>> {
-        let signature = self.signature();
-
-        if signature.len() < 3 {
-            return Err(serde::de::Error::invalid_length(
-                signature.len(),
-                &">= 2 characters",
-            ));
-        }
-
-        let mut bytes = signature.as_bytes().iter();
-        // We can't get None here cause we already established there are at least 2 chars above
-        let c = bytes.next().map(|b| *b as char).expect("empty signature");
-        if c != STRUCT_SIG_START_CHAR {
-            return Err(serde::de::Error::invalid_value(
-                serde::de::Unexpected::Char(c),
-                &crate::STRUCT_SIG_START_STR,
-            ));
-        }
-        if bytes.next().map(|b| *b as char).expect("empty signature") == STRUCT_SIG_END_CHAR {
-            return Err(serde::de::Error::invalid_value(
-                serde::de::Unexpected::Str("()"),
-                &"at least one field signature between `(` and `)`",
-            ));
-        }
-
-        let mut fields_sig_len = 0;
-        let mut fields_parser = self.slice(1..);
-        while !fields_parser.done() && fields_parser.next_char()? != STRUCT_SIG_END_CHAR {
-            fields_sig_len += fields_parser.parse_next_signature()?.len();
-        }
-        let c = fields_parser.next_char()?;
-        if c != STRUCT_SIG_END_CHAR {
-            return Err(serde::de::Error::invalid_value(
-                serde::de::Unexpected::Char(c),
-                &crate::STRUCT_SIG_END_STR,
-            ));
-        }
-
-        // The `(`, fields signatures and `)`.
-        Ok(self.signature_slice(0, fields_sig_len + 2))
-    }
-
-    fn next_dict_entry_signature(&self) -> Result<Signature<'_>> {
-        let signature = self.signature();
-
-        if signature.len() < 4 {
-            return Err(serde::de::Error::invalid_length(
-                signature.len(),
-                &">= 4 characters",
-            ));
-        }
-
-        // We can't get None here cause we already established there are at least 4 chars above
-        let bytes = signature.as_bytes();
-        let c = bytes.first().map(|b| *b as char).expect("empty signature");
-        if c != DICT_ENTRY_SIG_START_CHAR {
-            return Err(serde::de::Error::invalid_value(
-                serde::de::Unexpected::Char(c),
-                &crate::DICT_ENTRY_SIG_START_STR,
-            ));
-        }
-
-        let key_parser = self.slice(1..);
-        let key_signature = key_parser.next_signature()?;
-        // Key's signature will always be just 1 character.
-        if key_signature.len() != 1 {
-            return Err(serde::de::Error::invalid_length(
-                key_signature.len(),
-                &"dict-entry key's signature can only be a single character",
-            ));
-        }
-
-        // There should be one valid complete signature for value.
-        let value_parser = self.slice(2..);
-        let value_len = value_parser.next_signature()?.len();
-        // signature of value + `{` + 1 char of the key signature + `}`
-        let end = value_len + 3;
-
-        if signature.len() < end {
-            return Err(serde::de::Error::invalid_length(
-                signature.len(),
-                &format!(">= {end} characters").as_str(),
-            ));
-        }
-        if bytes[end - 1] as char != DICT_ENTRY_SIG_END_CHAR {
-            return Err(serde::de::Error::invalid_value(
-                serde::de::Unexpected::Char(c),
-                &crate::DICT_ENTRY_SIG_END_STR,
-            ));
-        }
-
-        Ok(self.signature_slice(0, end))
     }
 
     fn signature_slice(&self, idx: usize, end: usize) -> Signature<'_> {
         self.signature.slice(self.pos + idx..self.pos + end)
     }
+}
+
+fn is_basic_type_signature<I: AsChar>(input: I) -> bool {
+    match input.as_char() {
+        u8::SIGNATURE_CHAR
+        | bool::SIGNATURE_CHAR
+        | i16::SIGNATURE_CHAR
+        | u16::SIGNATURE_CHAR
+        | i32::SIGNATURE_CHAR
+        | u32::SIGNATURE_CHAR
+        | i64::SIGNATURE_CHAR
+        | u64::SIGNATURE_CHAR
+        | f64::SIGNATURE_CHAR
+        | <&str>::SIGNATURE_CHAR
+        | ObjectPath::SIGNATURE_CHAR
+        | Signature::SIGNATURE_CHAR
+        | VARIANT_SIGNATURE_CHAR => true,
+        #[cfg(unix)]
+        Fd::SIGNATURE_CHAR => true,
+        _ => false,
+    }
+}
+
+fn signature<I, E>(input: I) -> IResult<I, I, E>
+where
+    E: ParseError<I> + ContextError<I>,
+    I: Clone + Offset + Slice<RangeTo<usize>> + Slice<RangeFrom<usize>> + InputIter + InputLength,
+    <I as InputIter>::Item: AsChar,
+{
+    let array_signature = recognize(preceded(char(ARRAY_SIGNATURE_CHAR), cut(signature)));
+
+    let struct_signature = recognize(delimited(
+        char(STRUCT_SIG_START_CHAR),
+        cut(many1_count(signature)),
+        cut(char(STRUCT_SIG_END_CHAR)),
+    ));
+
+    let dict_entry_signature = recognize(delimited(
+        char(DICT_ENTRY_SIG_START_CHAR),
+        cut(pair(satisfy(is_basic_type_signature), signature)),
+        cut(char(DICT_ENTRY_SIG_END_CHAR)),
+    ));
+
+    #[cfg(feature = "gvariant")]
+    let maybe_signature = recognize(preceded(char(MAYBE_SIGNATURE_CHAR), cut(signature)));
+
+    #[cfg(not(feature = "gvariant"))]
+    let maybe_signature = nom::combinator::fail;
+
+    alt((
+        recognize(satisfy(is_basic_type_signature)),
+        array_signature,
+        struct_signature,
+        dict_entry_signature,
+        maybe_signature,
+    ))(input)
 }
